67fa5645ba5e43fb02128a06e90da1f10dfb24e2
eddy <medvednic@hottmail.com>
2016-06-29 16:29:04 +0300

    Initial commit

---
+++ app/src/main/java/eddy/customservice/MainActivity.java
@@ -0,0 +1,31 @@
+package eddy.customservice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvEmail)
+    TextView tvEmail;
+    @BindView(R.id.btRegister)
+    Button btRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.btRegister)
+    public void onClick() {
+
+    }
+}
