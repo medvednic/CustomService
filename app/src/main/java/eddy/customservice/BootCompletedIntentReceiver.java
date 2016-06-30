9bd9f58cc319b1bd4b5fcca4830b1138c61fd5e3
eddy <medvednic@hottmail.com>
2016-07-01 00:50:13 +0300

    Fixed issue of incorrect service restart after boot. added condition for users credentials in shared preferences.

--- app/src/main/java/eddy/customservice/BootCompletedIntentReceiver.java
+++ app/src/main/java/eddy/customservice/BootCompletedIntentReceiver.java
@@ -3,6 +3,7 @@ package eddy.customservice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 /**
  * Created by Eddy Medvednic on 30/06/2016.
@@ -11,8 +12,12 @@ public class BootCompletedIntentReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-            Intent pushIntent = new Intent(context, MenuService.class);
-            context.startService(pushIntent);
+            SharedPreferences users = context.getSharedPreferences("RegisteredUser", Context.MODE_PRIVATE);
+            String user = users.getString("emailAddress", "");
+            if (!user.equals("")){ //only start service if user is still logged in
+                Intent pushIntent = new Intent(context, MenuService.class);
+                context.startService(pushIntent);
+            }
         }
     }
 }
