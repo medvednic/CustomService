e90f72b41a65c50817d248e930af6fb86166bb8f
Eddy <medvednic@hotmail.com>
2016-06-30 03:05:21 +0300

    Service autostarts on device boot.

---
+++ app/src/main/java/eddy/customservice/BootCompletedIntentReceiver.java
@@ -0,0 +1,18 @@
+package eddy.customservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Eddy Medvednic on 30/06/2016.
+ */
+public class BootCompletedIntentReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+            Intent pushIntent = new Intent(context, MenuService.class);
+            context.startService(pushIntent);
+        }
+    }
+}
